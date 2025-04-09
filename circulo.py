97ceb268c5219e6d27fea1ae459beb890e7d3957
Brianna <61680023@continental.edu.pe>
2025-04-09 11:37:31 -0500

    Se implemento el area

--- circulo.py
+++ circulo.py
@@ -5,8 +5,12 @@ class Circulo:
     def circunferencia(self):
         return (2*self.PI*self.radio)
 
+    def area(self):
+        return self.PI*self.radio*self.radio
+
 if __name__ == "__main__":
     instancia_circulo = Circulo(10)
     print(f"La circunferencia es: {instancia_circulo.circunferencia()}")
+    print(f"El area es: {instancia_circulo.area()}")
 
 
